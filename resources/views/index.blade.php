853acb9200360b6dba4b9d47ff79ae8ea82e2665
tien95vt <ttienn123@gmail.com>
2017-10-18 10:45:01 +0700

    post index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -193,39 +193,45 @@
 		<div class="panel panel-primary" style="margin: 20px;">
 			<div class="panel-heading">NHÀ HÀNG </div>
 			<div class="panel-body">
-				@php $breakRow = 0; @endphp
+				@php $breakRow = 1; @endphp
 				<div class="row" style="margin: 5px;">
 					@foreach($postNhahang as $valuePostNhaHang)
 					<div class="col-xs-12 col-md-3" style="word-wrap: break-word; padding: 10px;">
-						<div class="post_item" style="border: 1px solid #ccc; padding: 10px;">
+						<div class="post_item">
 							<div class="row">
 								<div class="col-md-12">
 									<img src="{{asset('upload/picture/post/'). '/'. $valuePostNhaHang->photo}}" alt="no_pic" height="150px" width="100%">
 								</div>
 							</div>
 							<div class="row">
-								<div class="col-md-12" >
-									{{$valuePostNhaHang->title}}
+								<div class="col-md-12 title_post" >
+									<h4>{{$valuePostNhaHang->title}}</h4>
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-12">
-									{{$valuePostNhaHang->address}}
+									<i class="fa fa-globe" aria-hidden="true"></i><a href="" class="space_left">{{$valuePostNhaHang->website}}</a>
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-12">
+									<i class="fa fa-hourglass-half" aria-hidden="true"></i>
 									{{$valuePostNhaHang->open_time}} - {{$valuePostNhaHang->close_time}}
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-12">
-									So dien thoai
+									<i class="fa fa-phone-square" aria-hidden="true" style="margin-right: 5px"></i>{{$valuePostNhaHang->phone}}
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<i class="fa fa-location-arrow" aria-hidden="true" style="margin-right: 5px"></i>{{$valuePostNhaHang->address}}
 								</div>
 							</div>
 						</div>
 					</div>
-					@if($breakRow==3)
+					@if($breakRow%4 == 0)
 				</div> <div class="row" style="margin: 10px;">
 					@endif
 					@php $breakRow++ ; @endphp
@@ -240,30 +246,36 @@
 				<div class="row" style="margin: 5px;">
 					@foreach($postKhachSan as $valuePostKhachSan)
 					<div class="col-xs-12 col-md-3" style="word-wrap: break-word; padding: 10px;">
-						<div class="post_item" style="border: 1px solid #ccc; padding: 10px;">
+						<div class="post_item">
 							<div class="row">
 								<div class="col-md-12">
 									<img src="{{asset('upload/picture/post/'). '/'. $valuePostKhachSan->photo}}" alt="no_pic" height="150px" width="100%">
 								</div>
 							</div>
 							<div class="row">
-								<div class="col-md-12" >
-									{{$valuePostKhachSan->title}}
+								<div class="col-md-12 title_post" >
+									<h4>{{$valuePostKhachSan->title}}</h4>
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-12">
-									Dia chi
+									<i class="fa fa-globe" aria-hidden="true"></i><a href="" class="space_left">{{$valuePostKhachSan->website}}</a>
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-12">
-									gio mo cua
+									<i class="fa fa-hourglass-half" aria-hidden="true"></i>
+									{{$valuePostKhachSan->open_time}} - {{$valuePostKhachSan->close_time}}
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-12">
-									So dien thoai
+									<i class="fa fa-phone-square" aria-hidden="true" style="margin-right: 5px"></i>{{$valuePostKhachSan->phone}}
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<i class="fa fa-location-arrow" aria-hidden="true" style="margin-right: 5px"></i>{{$valuePostKhachSan->address}}
 								</div>
 							</div>
 						</div>
@@ -276,9 +288,57 @@
 				</div>
 			</div>
 		</div>
+
+		{{-- carousel --}}
+		{{-- <div class="panel panel-primary"  style="margin: 20px;">
+			<div class="panel-heading">Panel Header</div>
+			<div class="panel-body">
+				<div id="carousel-2"  class="carousel slide col-md-12" data-ride="carousel" data-type="multi" data-interval="10000" id="myCarousel">
+					<div class="carousel-inner">
+						@foreach($postNhahang as $key => $valuePostNhaHang)
+						<div class="item {{ $key == 0 ? ' active' : '' }} " >
+							<div class="col-md-3 col-sm-6 col-xs-12">
+								<a href="#"><img  src="{{asset('upload/picture/post/'). '/'. $valuePostNhaHang->photo}}" class="img-responsive"></a>
+								<div class="text-center">AAAAA</div>
+							</div>
+						</div>
+						@endforeach
+					</div>
+					<a class="left carousel-control" href="#carousel-2" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+					<a class="right carousel-control" href="#carousel-2" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+				</div>
+			</div>
+		</div> --}}
+		{{-- end carousel --}}
 	</div>		
+
+
+
 </section>
+
+{{-- <script>
+	$(document).ready(function(){
+		$('.carousel[data-type="multi"] .item').each(function(){
+			var next = $(this).next();
+			if (!next.length) {
+				next = $(this).siblings(':first');
+			}
+			next.children(':first-child').clone().appendTo($(this));
+
+			for (var i=0; i<1; i++) {
+				next=next.next();
+				if (!next.length) {
+					next = $(this).siblings(':first');
+				}
+
+				next.children(':first-child').clone().appendTo($(this));
+			}
+		});
+	});
+</script> --}}
 
 <!-- XONG PHẦN CONTENT -->
 @endsection
+
+
 
